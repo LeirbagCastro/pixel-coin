93985ea1ec6c946777cd96caed360a61ef888b80
LeirbagCastro <gabrielcmamede@gmail.com>
2025-09-29 15:31:12 -0300

    feat: Adds initial javascript structure

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const form = document.getElementById("converterForm")
+const amount = document.getElementById("amount")
+const fromCurrency = document.getElementById("fromCurrency")
+const convertedAmount = document.getElementById("convertedAmount")
+const toCurrency = document.getElementById("toCurrency")
+const loading = document.querySelector(".loading")
+const result = document.querySelector(".result")
+const error = document.querySelector(".error")
+
+const API_URL = "https://api.exchangerate-api.com/v4/latest/"
+
+async function convertMoney(){
+
+    loading.style.display = "block"
+    
+    try{
+    const response = await fetch(API_URL + fromCurrency.value)
+    const data = await response.json()
+
+    const rate = data.rates[toCurrency.value]
+    const convertedRate = (amount.value * rate).toFixed(2)
+    }
+    catch(error){
+        alert("Falha no servidor")
+    }
+}
+
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    convertMoney() 
+})
